56f863140d5b8cbab6acd5376345acc1fdcf0193
Stepan Nedrya <61784476+skaner27@users.noreply.github.com>
2020-06-23 00:51:10 +0300

    Update getuser.php

--- crud/getuser.php
+++ crud/getuser.php
@@ -6,10 +6,13 @@ include_once ("../lang/lang.".$_SESSION['NowLang'].".php");
 <html>
 <head>
 <style>
+    .table{
+        margin: 0px auto;
+        width: 900px;
+        border: 1px solid black;
 
-table, td, th {
-  border: 1px solid black;
-}
+    }
+
 
 th {text-align: left;}
 </style>
@@ -24,14 +27,18 @@ mysqli_select_db($connect,"ajax_demo");
 $sql="SELECT * FROM `users` WHERE id = '$q'";
 $result = mysqli_query($connect,$sql);
 
-echo "<table>" .
-    "<th>" . "ID" . "</th>" .
-"<th>" . $Lang['name_tabl'] . "</th>" .
-"<th>" . $Lang['surname_tabl'] . "</th>" .
-"<th>" . $Lang['login_lan'] . "</th>" .
-"<th>" . $Lang['pass_lan'] . "</th>" .
-"<th>" . $Lang['lang_tabl'] . "</th>" .
-"<th>" . $Lang['role_tabl'] . "</th>";
+echo "<table class= 'table table-dark'>";
+        echo '<thead>';
+    echo '<tr>'.
+        "<th scope='col'>" . "ID" . "</th>" .
+        "<th scope='col'>" . $Lang['name_tabl'] . "</th>" .
+        "<th scope='col'>" . $Lang['surname_tabl'] . "</th>" .
+        "<th scope='col'>" . $Lang['login_lan'] . "</th>" .
+        "<th scope='col'>" . $Lang['pass_lan'] . "</th>" .
+        "<th scope='col'>" . $Lang['lang_tabl'] . "</th>" .
+        "<th scope='col'>" . $Lang['role_tabl'] . "</th>";
+echo '<tr>';
+echo '</thead>';
 
 while($row = mysqli_fetch_array($result)) {
   echo "<tr>";

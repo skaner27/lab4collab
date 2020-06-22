ef20267979529215a2a4944b4f9a2dccce0a644a
Stepan Nedrya <61784476+skaner27@users.noreply.github.com>
2020-06-23 00:48:38 +0300

    Update manager.php

--- roles/manager.php
+++ roles/manager.php
@@ -5,14 +5,35 @@ class Manager  extends User
     public function managerGreeting()
     {
         if ($_SESSION['NowLang'] == "ru") {
-            echo "Здравствуйте, Менеджер " . $this->name . " " . $this->surname . ". Вы можете на сайте изменять и создавать клиентов.";
-            echo '<a href="../crud/crud.php" method="POST">Продолжить</a>';
+            echo '<div class="alert alert-dark" role="alert">' . "Здравствуйте, Менеджер " . $this->name . " " . $this->surname . ". Вы можете на сайте изменять и создавать клиентов.";echo '</div>';
+            echo '<div class="btn1">' . '<a href = "../crud/crud.php">
+        <br><button type="button" class="btn btn-outline-dark">' . 'Продолжить' . '</button>' . '</a>';
         } elseif ($_SESSION['NowLang'] == "ua") {
-            echo "Вітаю, Менеджер " . $this->name . " " . $this->surname . ". Ви можете на сайті змінювати, видаляти і створювати клієнтів.";
-            echo '<a href="../crud/crud.php" method="POST">Продовжити</a>';
+            echo '<div class="alert alert-dark" role="alert">' . "Вітаю, Менеджер " . $this->name . " " . $this->surname . ". Ви можете на сайті змінювати, видаляти і створювати клієнтів.";echo '</div>';
+            echo '<div class="btn1">' . '<a href = "../crud/crud.php">
+        <br><button type="button" class="btn btn-outline-dark">' . 'Продовжити' . '</button>' . '</a>';
         } else {
-            echo "Hello, Manager " . $this->name . " " . $this->surname . ". You can modify, delete and create clients on the site.";
-            echo '<a href="../crud/crud.php" method="POST">Continue</a>';
+            echo '<div class="alert alert-dark" role="alert">' . "Hello, Manager " . $this->name . " " . $this->surname . ". You can modify, delete and create clients on the site.";echo '</div>';
+            echo '<div class="btn1">' . '<a href = "../crud/crud.php">
+        <br><button type="button" class="btn btn-outline-dark">' . 'Continue' . '</button>' . '</a>';
         }
     }
 }
+?>
+<html>
+<head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <title><?php echo $Lang['title']?></title>
+<style>
+    .alert{
+        text-align: center;
+        margin: 0px auto;
+        width: 900px;
+    }
+    .btn1{
+        text-align: center;
+    }
+</style>
+</head>
+</html>

c55512d142a7c4cf5670f8550d3b6cc4e78bdbd3
Stepan Nedrya <61784476+skaner27@users.noreply.github.com>
2020-06-25 12:53:52 +0300

    Update crud.php

--- crud/crud.php
+++ crud/crud.php
@@ -32,20 +32,7 @@ if(isset($_GET['del_tab_us'])){
     $result_del->bindParam(':id', $id, PDO::PARAM_INT);
     $result_del->execute();
 }
-
-try
-{
-    $sql = "SELECT id, login, password, role, name, surname, ip FROM `users` WHERE `login`= '$login' AND `password`='$password'";
-    $result = $pdo->query($sql);
-    $row = $result->fetch();
-
-}
-catch (PDOException $e)
-{
-    $output = 'Ошибка при выполнении обновления: ' . $e->getMessage();
-    echo $output;
-    exit();
-}
+require_once '$sql_res.php';
 
 try{
 

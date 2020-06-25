326b931b2a6cd3959d4714c8cad3bcc69466a32d
Stepan Nedrya <61784476+skaner27@users.noreply.github.com>
2020-06-25 12:49:22 +0300

    Update $sql_res.php

--- crud/$sql_res.php
+++ crud/$sql_res.php
@@ -1,1 +1,14 @@
+<?php
+try
+{
+    $sql = "SELECT id, login, password, role, name, surname, ip FROM users WHERE login= '$login' AND password='$password'";
+    $result = $pdo->query($sql);
+    $row = $result->fetch();
 
+}
+catch (PDOException $e)
+{
+    $output = 'Ошибка при выполнении обновления: ' . $e->getMessage();
+    echo $output;
+    exit();
+}
